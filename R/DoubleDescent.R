1f78cce26474373a69c0d4a11150532aee0e3d99
Norm Matloff <nsmatloff@ucdavis.edu>
2022-04-07 15:42:54 -0700

    starting

---
+++ R/DoubleDescent.R
@@ -0,0 +1,25 @@
+
+# facilitates exploration of the Double Descent phenomenon
+
+# see
+
+## https://matloff.wordpress.com/2020/11/11/the-notion-of-double-descent/
+
+## https://twitter.com/matloff/status/1507935158648782856
+
+# arguments:
+
+#    data,yName:  as in other qe* functions
+#    xPts:  values on the X-axis showing where to run the code, e.g.
+#       degree of polynomial
+#    nReps: number of runs of the experiment
+#    qeName: name of qe* function to run
+#    qeArgs: R list showing the qe* ftn args to use INSTEAD of the
+#       default
+
+# will plot testAcc and trainAcc, in red and blue
+
+doubleD <- function(data,yName,xPts,nReps,qeName,qeArgs)
+{
+
+}
