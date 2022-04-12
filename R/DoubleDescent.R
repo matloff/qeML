882e05ffed0a40fea8863c4e17bcb1f318b55f60
Norm Matloff <nsmatloff@ucdavis.edu>
2022-04-12 16:40:50 -0700

    added makeDummies arg

--- R/DoubleDescent.R
+++ R/DoubleDescent.R
@@ -7,24 +7,48 @@
 
 ## https://twitter.com/matloff/status/1507935158648782856
 
+# examples: 
+
+#   polynomial model, varying degree; pef dataset from this package
+
+#      cmd <- 'qePolyLin(pef,"wageinc",deg=xPts[i]`)'
+#      z <- doubleD(cmd,c(1:4),100)
+#      plot(z)
+
+#   fixed model, vary n; here the data is modified from the crdtdataset
+#   in the ISLR pkg
+
+#      cmd <- 'qeLin(crd,"Balance",holdout=nrow(crd)-xPts[i])'
+#      z <- doubleD(cmd,seq(50,3,-1),250)
+#      plot(z)
+
+#   for any i, the model will be fit on n = xPts[i] data points, and then
+#   tested on the remaining data, the holdout
+
 # arguments:
 
 #    qeFtnCall: quoted string; somewhere should include 'xPts[i]'
 #    xPts:  values on the X-axis showing where to run the code, e.g.
-#       degree of polynomial
+#       various values of degree of polynomial
 #    nReps: number of runs of the experiment
+#    makeDummies: call regtools::factorsToDummies() before running
+#       the experiments (to avoid "new factor level found" when
+#       predicting the test set; if used, must be set to the nam of the
+#       dataset, as shown in cmd
 
 # will plot testAcc and trainAcc, in red and blue
 
-# example: 
-
-#    cmd <- 'qePolyLin(pef,"wageinc",deg=xPts[i]`)'
-#    z <- doubleD(cmd,c(1:4),100)
-
-doubleD <- function(qeFtnCall,xPts,nReps,classif=FALSE)
+doubleD <- function(qeFtnCall,xPts,nReps,makeDummies=NULL,classif=FALSE)
 {
-   warning('still experimental')
-   if (classif) stop('not set for classification problems yet')
+   if (!is.null(makeDummies)) {
+      dta <- makeDummies
+      cmd <- 
+         paste0(dta,' <- factorsToDummies(',dta,
+         ',omitLast=TRUE)')
+      eval(parse(text=cmd))
+      cmd <- paste0(dta, ' <- as.data.frame(',dta,')')
+      eval(parse(text=cmd))
+   }
    cmd <- paste0('tmp <- ',cmd,'; c(tmp$testAcc,tmp$trainAcc)')
    res <- matrix(nrow=length(xPts),ncol=3)
    res[,1] <- xPts
