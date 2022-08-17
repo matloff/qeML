b8cba075aea19b3dcb978e1029e3ea6aa05b7c7f
Norm Matloff <nsmatloff@ucdavis.edu>
2022-08-16 19:58:32 -0700

    cran prep

--- R/Quick.R
+++ R/Quick.R
@@ -590,7 +590,7 @@ qeRFgrf <- function(data,yName,nTree=2000,minNodeSize=5,
             grf::regression_forest(x,y,num.trees=nTree,min.node.size=minNodeSize,
             mtry=mtry)
          else 
-            ll_grf::regression_forest(x,y,
+            grf::regression_forest(x,y,
             num.trees=nTree,min.node.size=minNodeSize,mtry=mtry,
             ll.split.lambda=lambda,ll.split.cutoff=splitCutoff)
    } else {
@@ -2205,7 +2205,7 @@ qeROC <- function(dataIn,qeOut,yName,yLevelName)
    if (is.null(probs)) stop('no holdoutPreds$probs')
    probs <- probs[,yLevelName]
    probs <- probs/sum(probs)
-   pRoc::roc(ys,probs,plot=T,aug=T)
+   pROC::roc(ys,probs,plot=T,aug=T)
 }
 
 ######################  qeToweranNA()  #############################
