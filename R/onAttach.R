7152b4849df7c46d85fd89eb36a20d88350319e6
Norm Matloff <nsmatloff@ucdavis.edu>
2022-12-28 11:01:27 -0800

    cran

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 
 .onAttach <- function(libname, pkgname) {
    packageStartupMessage(
-      '\n\n\n\n\n*********************\n\n\n\nnType vignette('QuickIntro') for a quick overview!\n\n\n\n')
+      '\n\n\n\n\n*********************\n\n\n\nType vignette('QuickIntro') for a quick overview!\n\n\n\n')
 }
 
