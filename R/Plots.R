f8bf41fc9ad9f51f925094de6c7fe6107e46c33d
Norm Matloff <nsmatloff@ucdavis.edu>
2023-01-10 23:29:14 -0800

    ftns for dim red etc.

---
+++ R/Plots.R
@@ -0,0 +1,17 @@
+
+# misc. plots, especially for dimension reduction
+
+plotClassesUMAP <- function(data,classVar) 
+{
+   if (is.character(classVar)) {  # name of a column in 'data'
+      whichClass <- which(names(data) == classVar)
+      x <- data[,-whichClass]
+      classVar <- data[,whichClass]
+   } else {
+      x <- data
+   }
+   if (!is.factor(classVar)) classVar <- as.factor(classVar)
+   if (!allNumeric(x)) x <- factorsToDummies(x)
+   ump <- uwot::umap(x)
+   plot(ump[,1:2],col=classVar)
+}
