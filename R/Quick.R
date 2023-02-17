ba497d17ba97fa56a3507d6ab716a8517feee283
Norm Matloff <nsmatloff@ucdavis.edu>
2023-02-17 14:49:34 -0800

    qeKNN fixed but checking other cases

--- R/Quick.R
+++ R/Quick.R
@@ -417,6 +417,7 @@ qeKNN <- function(data,yName,k=25,scaleX=TRUE,
    class(knnout) <- c('qeKNN','kNN')
    if (!is.null(holdout)) {
       yName <- 'yTst'
+      tst[,ycol] <- y[holdIdxs]
       predictHoldoutKNN(knnout)
       knnout$holdIdxs <- holdIdxs
    } else knnout$holdIdxs <- NULL
