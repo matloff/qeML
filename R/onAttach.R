b4737efd9d13d38dbc456406772246899f7081e1
Norm Matloff <nsmatloff@ucdavis.edu>
2023-03-10 09:36:05 -0800

    cran

--- R/onAttach.R
+++ R/onAttach.R
@@ -2,8 +2,8 @@
 .onAttach <- function(libname, pkgname) {
    packageStartupMessage(
       '\n\n\n\n\n*********************\n\n\n\n  Navigating qeML:\n
-      Type vignette("QuickStart") for a quick overview!\n
+      Type vignette("Quick_Start") for a quick overview!\n
       Type vignette("FtnList") for a categorized function list\n
-      Type vignette("MLOverview") for an introduction to machine learning')
+      Type vignette("ML_Overview") for an introduction to machine learning')
 }
 
