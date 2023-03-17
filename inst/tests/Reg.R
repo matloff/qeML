1c9c198562182c54e0a2f85f5b8f43609be6b0cc
Norm Matloff <nsmatloff@ucdavis.edu>
2023-03-17 15:56:56 -0700

    CRAN

--- inst/tests/Reg.R
+++ inst/tests/Reg.R
@@ -1,2 +1,45 @@
 
+regFtns <- c(
+'qeDT',
+'qeGBoost',
+'qeLightGBoost',
+'qeRF',
+'qeRFgrf',
+'qeRFranger',
+'qeXGBoost',
+'qeKNN',
+'qeKNNna',
+'qeLinKNN',
+'qePolyLinKNN',
+'qeDeepnet',
+'qeNeural',
+'qeLASSO',
+'qeLin',
+'qeNCVregCV',
+'qePolyLASSO',
+'qePolyLin',
+'qePolyLog',
+'qeSVM',
+'qeliquidSVM'
+)
+
+require(qeML)
+data(prgeng)
+set.seed(9999)
+pe1000 <- prgeng[sample(1:nrow(prgeng),1000),]
+
+evalr <- function(toexec) {
+   eval(parse(text=toexec),parent.frame())
+}
+
+checkAll <- function() 
+{
+   for (qeFtn in regFtns) {
+      qeCmd <- sprintf('qeOut <- %s(pe1000,"wageinc")$testAcc',qeFtn)
+      evalr(qeCmd)
+      print(qeFtn)
+      print(qeOut)
+      ans <- readline('next')
+   }
+}
 
