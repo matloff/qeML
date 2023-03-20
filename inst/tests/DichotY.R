7b6905641236fe73929257fcff84f5a4b512f1aa
Norm Matloff <nsmatloff@ucdavis.edu>
2023-03-20 15:16:29 -0700

    CRAN

---
+++ inst/tests/DichotY.R
@@ -0,0 +1,50 @@
+
+regFtns <- c(
+'qeAdaBoost',
+'qeDT',
+'qeGBoost',
+'qeLightGBoost',
+'qeRF',
+'qeRFgrf',
+'qeRFranger',
+'qeXGBoost',
+'qeKNN',
+'qeLinKNN',
+'qePolyLinKNN',
+'qeDeepnet',
+'# qeNeural',
+'qeLASSO',
+'qeLin',
+'qeLogit',
+'qeNCVregCV',
+'qePolyLASSO',
+'qePolyLin',
+'# qePolyLog',
+'qeSVM',
+'qeliquidSVM'
+)
+
+require(qeML)
+data(prgeng)
+set.seed(9999)
+pe1000 <- prgeng[sample(1:nrow(prgeng),1000),]
+
+evalr <- function(toexec) {
+   eval(parse(text=toexec),parent.frame())
+}
+
+checkAll <- function() 
+{
+   for (qeFtn in regFtns) {
+      if (substr(qeFtn,1,1) == '#') {
+         warning(paste0('skipping ',qeFtn))
+         next
+      }
+      qeCmd <- sprintf('qeOut <- %s(pe1000,"sex")$testAcc',qeFtn)
+      evalr(qeCmd)
+      print(qeFtn)
+      print(qeOut)
+      ans <- readline('next')
+   }
+}
+
