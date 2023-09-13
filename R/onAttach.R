1eb55b93e14f9a2f947601b994b84280ea6a6266
Norm Matloff <nsmatloff@ucdavis.edu>
2023-09-13 11:00:20 -0700

    cran

--- R/onAttach.R
+++ R/onAttach.R
@@ -3,7 +3,7 @@
    packageStartupMessage(
       '\n\n\n\n\n*********************\n\n\n\n  Navigating qeML:\n
       Type vignette("Quick_Start") for a quick overview!\n
-      Type vignette("FtnList") for a categorized function list\n
+      Type vignette("Function_List") for a categorized function list\n
       Type vignette("ML_Overview") for an introduction to machine learning')
 }
 
