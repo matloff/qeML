155aaf0d49e19786b748027957c64965a2edd580
Norm Matlof <nsmatloff@ucdavis.edu>
2024-10-31 13:31:00 -0700

    need man pg, NAMESPACE

---
+++ mittal.R
@@ -0,0 +1,34 @@
+
+
+# plots several curves, against a common X-axis, as in qePlotCurves, but
+# showing the change in each variable, relative to the variable's value
+# at min X
+
+# 'data' must be a data frame or equivalent, with this structure
+
+#  column 1 is the "X" variable to serve in the output plot
+
+#  the remaining columns will serve as "Y" columns, one for each curve
+#  to be plotted,  divided by the value of this
+#  variable at min X; it is presumed that the sequence is sorted in
+#  ascending order, with the first element corresponding to X = 0
+
+
+qeMittalGraph <- function(data) 
+{
+
+   x <- data[,1]
+   nc <- ncol(data)
+   # reshape, esp. including the curve number
+   argMinX <- which.min(data$x)
+   z <- lapply(1:(nc-1),
+      function(i) {
+         tmp <- data[,i+1] / data[argMinX,i+1]
+         tmpDF <- data.frame(x=x,curveNum=i,y=tmp)
+         tmpDF
+      }
+   )
+   zz <- do.call(rbind,z)
+   qePlotCurves(zz,1,3,2)
+}
+
