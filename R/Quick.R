348ddb9f931849eb729eab9b63ae35f9e8f7c6c3
Norm Matloff <nsmatloff@ucdavis.edu>
2026-03-31 10:23:29 -0700

    added regr case to qeDeepnet

--- R/Quick.R
+++ R/Quick.R
@@ -1241,7 +1241,7 @@ qeLightGBoost <- function(data,yName,nTree=100,minNodeSize=10,learnRate=0.1,
    
    outlist$nTree <- nTree
    outlist$trainRow1 <- data[1,-ycol]
-   class(outlist) <- c('qelightGBoost')
+   class(outlist) <- c('qeLightGBoost')
 
    if (!is.null(holdout)) {
       # predictHoldout(outlist)
@@ -1388,7 +1388,7 @@ qeNeural <- function(data,yName,hidden=c(100,100),nEpoch=30,
    acts=rep("relu",length(hidden)),learnRate=0.001,conv=NULL,xShape=NULL,
    holdout=floor(min(1000,0.1*nrow(data))))
 {
-   checkPkgLoaded('keras')
+   checkPkgLoaded('keras3')
    yNameSave <- yName
    checkForNonDF(data)
    # for use with qeRT(), hidden could be a string
@@ -1396,8 +1396,8 @@ qeNeural <- function(data,yName,hidden=c(100,100),nEpoch=30,
       hidden <- as.numeric(strsplit(hidden,',')[[1]])
 
    classif <- is.factor(data[[yName]])
-   requireNamespace('keras')
-   py_require_legacy_keras()     
+   requireNamespace('keras3')
+   # py_require_legacy_keras()     
    holdIdxs <- tst <- trn <- NULL  # for CRAN "unbound globals" complaint
    if (!is.null(holdout)) splitData(holdout,data)
    ycol <- which(names(data) == yName)
@@ -2539,81 +2539,74 @@ predict.qeXGBoost <- function(object,x,...)
 
 #######################  qeDeepnet()()  ##############################
 
-# Deepnet
 
-# using almost all available arguments from the wrappee, but not
-# allowing regression cases, on which the package seems to behave
-# erraticly
- 
-qeDeepnet <- function(data,yName,hidden=c(10),activationfun='sigm',
-   learningrate=0.8,momentum=0.5,learningrate_scale=1,
-   numepochs=3,batchsize=100,hidden_dropout=0,
-   yesYVal=NULL,holdout=floor(min(1000,0.1*nrow(data))))
+qeDeepnet <- function (data, yName, hidden = c(100,100), 
+   activationfun="sigm",learningrate=0.001,momentum=0.5,
+   learningrate_scale=1,numepochs=100,batchsize=25,
+   hidden_dropout=0,yesYVal=NULL,
+   holdout=floor(min(1000,0.1*nrow(data))))
 {
-   checkPkgLoaded('deepnet')
-   checkForNonDF(data)
-   trainRow1 <- getRow1(data,yName)
-   classif <- is.factor(data[[yName]])
-   if (!classif) stop('set up for classification problems only')
-   ycol <- which(names(data) == yName)
-   x <- data[,-ycol,drop=FALSE]
-   y <- data[,yName]
-   yLevels <- levels(y)
-   y <- regtools::factorsToDummies(y,omitLast=FALSE)
-   output <- 'sigm'
-   
-   if (!allNumeric(x)) {
-      x <- regtools::factorsToDummies(x,omitLast=TRUE)
-      factorsInfo <- attr(x,'factorsInfo') 
-   } else factorsInfo <- NULL
-
-   holdIdxs <- tst <- trn <- NULL  # for CRAN "unbound globals" complaint
-   if (!is.null(holdout)) {
-      holdIdxs <- sample(1:nrow(data),holdout)
-      tsty <- y[holdIdxs]
-      tstx <- x[holdIdxs,,drop=FALSE]
-      trny <- y[-holdIdxs]
-      trnx <- x[-holdIdxs,,drop=FALSE]
-   } else {
-      trnx <- x
-      trny <- y
-   }
-   # if holdout, x,y are now the training set
-
-   xm <- as.matrix(x)
-   nnOut <- deepnet::nn.train(x=xm,y=y,hidden=hidden, 
-      activationfun=activationfun,learningrate=learningrate, 
-      momentum=momentum,learningrate_scale=learningrate_scale,
-      output=output,numepochs=numepochs,batchsize=batchsize, 
-      hidden_dropout=hidden_dropout) 
-   class(nnOut) <- c('qeDeepnet')
-
-   nnOut$classif <- classif
-   nnOut$factorsInfo <- factorsInfo
-   nnOut$yLevels <- yLevels
-
-   if (!is.null(holdout)) {
-      preds <- predict(nnOut,tstx)
-      nnOut$testAcc <- mean(preds$predClasses != data[holdIdxs,ycol])
-      nnOut$holdIdxs <- holdIdxs
+    checkPkgLoaded("deepnet")
+    checkForNonDF(data)
+    trainRow1 <- qeML:::getRow1(data, yName)
+    classif <- is.factor(data[[yName]])
+    ycol <- which(names(data) == yName)
+    x <- data[, -ycol, drop = FALSE]
+    y <- data[, yName]
+    yLevels <- levels(y)
+    if (classif) y <- regtools::factorsToDummies(y, omitLast = FALSE)
+    output <- "linear"
+    if (!allNumeric(x)) {
+        x <- regtools::factorsToDummies(x, omitLast = TRUE)
+        factorsInfo <- attr(x, "factorsInfo")
     }
-   else nnOut$holdIdxs <- NULL
-
-   nnOut
-
+    else factorsInfo <- NULL
+    holdIdxs <- tst <- trn <- NULL
+    if (!is.null(holdout)) {
+        holdIdxs <- sample(1:nrow(data), holdout)
+        tsty <- y[holdIdxs]
+        tstx <- x[holdIdxs, , drop = FALSE]
+        trny <- y[-holdIdxs]
+        trnx <- x[-holdIdxs, , drop = FALSE]
+    }
+    else {
+        trnx <- x
+        trny <- y
+    }
+    xm <- as.matrix(x)
+    nnOut <- deepnet::nn.train(x = xm, y = y, hidden = hidden, 
+        activationfun = activationfun, learningrate = learningrate, 
+        momentum = momentum, learningrate_scale = learningrate_scale, 
+        output = output, numepochs = numepochs, batchsize = batchsize, 
+        hidden_dropout = hidden_dropout)
+    class(nnOut) <- c("qeDeepnet")
+    nnOut$classif <- classif
+    nnOut$factorsInfo <- factorsInfo
+    nnOut$yLevels <- yLevels
+    if (!is.null(holdout)) {
+        preds <- predict(nnOut, tstx)
+        nnOut$testAcc <- 
+           if (classif) mean(preds$predClasses != data[holdIdxs,ycol])
+           else mean(abs(tsty-preds))
+        nnOut$holdIdxs <- holdIdxs
+    }
+    else nnOut$holdIdxs <- NULL
+    nnOut
 }
 
-predict.qeDeepnet <- function(object,newx,...) 
+predict.qeDeepnet <- function (object, newx, ...) 
 {
-   if (!allNumeric(newx)) 
-      newx <- regtools::factorsToDummies(newx,omitLast=TRUE,
-         factorsInfo=object$factorsInfo)
-   else newx <- as.matrix(newx)
-   probs <- deepnet::nn.predict(object,newx)
-   colnames(probs) <- object$yLevels
-   predClassIdxs <- apply(probs,1,which.max)
-   predClasses <- object$yLevels[predClassIdxs]
-   list(predClasses=predClasses,probs=probs)
+    if (!allNumeric(newx)) 
+        newx <- regtools::factorsToDummies(newx, omitLast = TRUE, 
+            factorsInfo = object$factorsInfo)
+    else newx <- as.matrix(newx)
+    if (object$classif) {
+       probs <- deepnet::nn.predict(object, newx)
+       colnames(probs) <- object$yLevels
+       predClassIdxs <- apply(probs, 1, which.max)
+       predClasses <- object$yLevels[predClassIdxs]
+       list(predClasses = predClasses, probs = probs)
+    } else deepnet::nn.predict(object, newx)
 }
 
 #######################  qeNCVreg()  ##############################
